0148652c9bdf5921d4cc847657cb9b99200a2d13
Matthew Leon <ml@matthewleon.com>
2017-12-04 21:57:10 -0500

    adjust ST spec indentation level

--- test/Test/Data/ArrayBuffer/Safe/TypedArray/ST.purs
+++ test/Test/Data/ArrayBuffer/Safe/TypedArray/ST.purs
@@ -13,32 +13,33 @@ import Test.Spec (Spec, describe, it)
 import Test.Spec.QuickCheck (QCRunnerEffects, quickCheck)
 
 testSTTypedArray :: Spec (QCRunnerEffects ()) Unit
-testSTTypedArray = describe "newSTTypedArray" $ do
-  describe "Int8Array" $ do
-    it "creates an empty array of the desired size" $
-      quickCheck \sz ->
-        let posSz = abs sz `mod` 10000
-        in  (unsafePartial $ fromJust
-             $ pureST (traverse unsafeFreeze =<< newSTTypedArray posSz)
-            )
-            `eq` (unsafePartial $ fromJust $ newTypedArray posSz) :: Int8Array
-    it "returns Nothing on attempts to make negatively sized arrays" $
-      quickCheck \sz ->
-        let posSz = - (abs sz)
-        in  isNothing
-              ((pureST (traverse unsafeFreeze =<< newSTTypedArray posSz))
-                :: Maybe Int8Array)
-  describe "Uint32Array" $ do
-    it "creates an empty array of the desired size" $
-      quickCheck \sz ->
-        let posSz = abs sz `mod` 10000
-        in  (unsafePartial $ fromJust
-             $ pureST (traverse unsafeFreeze =<< newSTTypedArray posSz)
-            )
-            `eq` (unsafePartial $ fromJust $ newTypedArray posSz) :: Uint32Array
-    it "returns Nothing on attempts to make negatively sized arrays" $
-      quickCheck \sz ->
-        let posSz = - (abs sz)
-        in  isNothing
-              ((pureST (traverse unsafeFreeze =<< newSTTypedArray posSz))
-                :: Maybe Uint32Array)
+testSTTypedArray = describe "STTypedArray" $ do
+  describe "newSTTypedArray" $ do
+    describe "Int8Array" $ do
+      it "creates an empty array of the desired size" $
+        quickCheck \sz ->
+          let posSz = abs sz `mod` 10000
+          in  (unsafePartial $ fromJust
+               $ pureST (traverse unsafeFreeze =<< newSTTypedArray posSz)
+              )
+              `eq` (unsafePartial $ fromJust $ newTypedArray posSz) :: Int8Array
+      it "returns Nothing on attempts to make negatively sized arrays" $
+        quickCheck \sz ->
+          let posSz = - (abs sz)
+          in  isNothing
+                ((pureST (traverse unsafeFreeze =<< newSTTypedArray posSz))
+                  :: Maybe Int8Array)
+    describe "Uint32Array" $ do
+      it "creates an empty array of the desired size" $
+        quickCheck \sz ->
+          let posSz = abs sz `mod` 10000
+          in  (unsafePartial $ fromJust
+               $ pureST (traverse unsafeFreeze =<< newSTTypedArray posSz)
+              )
+              `eq` (unsafePartial $ fromJust $ newTypedArray posSz) :: Uint32Array
+      it "returns Nothing on attempts to make negatively sized arrays" $
+        quickCheck \sz ->
+          let posSz = - (abs sz)
+          in  isNothing
+                ((pureST (traverse unsafeFreeze =<< newSTTypedArray posSz))
+                  :: Maybe Uint32Array)
