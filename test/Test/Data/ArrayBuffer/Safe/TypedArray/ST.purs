ed9f1163af133c5cbbe31fd85be3a0069b8778cc
Matthew Leon <ml@matthewleon.com>
2017-12-04 21:51:24 -0500

    ST tests for Uint32Array

--- test/Test/Data/ArrayBuffer/Safe/TypedArray/ST.purs
+++ test/Test/Data/ArrayBuffer/Safe/TypedArray/ST.purs
@@ -3,7 +3,7 @@ module Test.Data.ArrayBuffer.Safe.TypedArray.ST where
 import Prelude hiding (eq)
 
 import Control.Monad.ST (pureST)
-import Data.ArrayBuffer.Safe.TypedArray (Int8Array, eq, newTypedArray)
+import Data.ArrayBuffer.Safe.TypedArray (Int8Array, Uint32Array, eq, newTypedArray)
 import Data.ArrayBuffer.Safe.TypedArray.ST (newSTTypedArray, unsafeFreeze)
 import Data.Maybe (Maybe, fromJust, isNothing)
 import Data.Ord (abs)
@@ -28,3 +28,17 @@ testSTTypedArray = describe "newSTTypedArray" $ do
         in  isNothing
               ((pureST (traverse unsafeFreeze =<< newSTTypedArray posSz))
                 :: Maybe Int8Array)
+  describe "Uint32Array" $ do
+    it "creates an empty array of the desired size" $
+      quickCheck \sz ->
+        let posSz = abs sz `mod` 10000
+        in  (unsafePartial $ fromJust
+             $ pureST (traverse unsafeFreeze =<< newSTTypedArray posSz)
+            )
+            `eq` (unsafePartial $ fromJust $ newTypedArray posSz) :: Uint32Array
+    it "returns Nothing on attempts to make negatively sized arrays" $
+      quickCheck \sz ->
+        let posSz = - (abs sz)
+        in  isNothing
+              ((pureST (traverse unsafeFreeze =<< newSTTypedArray posSz))
+                :: Maybe Uint32Array)
