c2e82915816131bfaa180314e18bc13f05754b7a
Matthew Leon <ml@matthewleon.com>
2017-12-05 19:17:20 -0500

    simplify types in TypedArray.ST

--- src/Data/ArrayBuffer/Safe/TypedArray/ST.purs
+++ src/Data/ArrayBuffer/Safe/TypedArray/ST.purs
@@ -39,26 +39,26 @@ foreign import newSTTypedArrayImpl
 -- | Read the value at the specified index in a mutable array.
 peek
   :: forall t m h r
-   . IsArrayType (ArrayView t) m
-  => STTypedArray h (ArrayView t)
+   . IsArrayType t m
+  => STTypedArray h t
   -> Int
   -> Eff (st :: ST h | r) (Maybe m)
 peek = peekImpl Just Nothing
 
 foreign import peekImpl
   :: forall t m h e r
-   . IsArrayType (ArrayView t) m
+   . IsArrayType t m
   => (m -> r)
   -> r
-  -> STTypedArray h (ArrayView t)
+  -> STTypedArray h t
   -> Int
   -> (Eff (st :: ST h | e) r)
 
 -- | Change the value at the specified index in a mutable array.
 foreign import poke
   :: forall t m h r
-   . IsArrayType (ArrayView t) m
-  => STTypedArray h (ArrayView t)
+   . IsArrayType t m
+  => STTypedArray h t
   -> Int
   -> m
   -> Eff (st :: ST h | r) Boolean
@@ -68,35 +68,35 @@ foreign import poke
 -- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray/fill
 foreign import fill
   :: forall t m h r
-   . IsArrayType (ArrayView t) m
-  => STTypedArray h (ArrayView t)
+   . IsArrayType t m
+  => STTypedArray h t
   -> m
-  -> Eff (st :: ST h | r) (STTypedArray h (ArrayView t))
+  -> Eff (st :: ST h | r) (STTypedArray h t)
 
 foreign import fillFrom
   :: forall t m h r
-   . IsArrayType (ArrayView t) m
-  => STTypedArray h (ArrayView t)
+   . IsArrayType t m
+  => STTypedArray h t
   -> Int
   -> m
-  -> Eff (st :: ST h | r) (STTypedArray h (ArrayView t))
+  -> Eff (st :: ST h | r) (STTypedArray h t)
 
 foreign import fillFromTo
   :: forall t m h r
-   . IsArrayType (ArrayView t) m
-  => STTypedArray h (ArrayView t)
+   . IsArrayType t m
+  => STTypedArray h t
   -> Int
   -> Int
   -> m
-  -> Eff (st :: ST h | r) (STTypedArray h (ArrayView t))
+  -> Eff (st :: ST h | r) (STTypedArray h t)
 
 -- | O(1). Convert a mutable array to an immutable array, without copying. The mutable
 -- | array must not be mutated afterwards.
-unsafeFreeze :: forall t h r. STTypedArray h (ArrayView t) -> Eff (st :: ST h | r) (ArrayView t)
-unsafeFreeze = pure <<< (unsafeCoerce :: STTypedArray h (ArrayView t) -> ArrayView t)
+unsafeFreeze :: forall t h r. STTypedArray h t -> Eff (st :: ST h | r) t
+unsafeFreeze = pure <<< (unsafeCoerce :: STTypedArray h t -> t)
 
 -- | Create an immutable copy of a mutable array.
-freeze :: forall t h r. STTypedArray h (ArrayView t) -> Eff (st :: ST h | r) (ArrayView t)
+freeze :: forall t h r. STTypedArray h t -> Eff (st :: ST h | r) t
 freeze = copyImpl
 
 -- | Create a mutable copy of an immutable typed array.
